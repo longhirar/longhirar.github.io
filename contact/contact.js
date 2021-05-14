c0930a76af30c262399e94e0d91d3888e6638e53
Longhirar <longhirar@gmail.com>
2021-05-14 13:46:44 -0300

    Update Contact Page
    Removed old insecure webhook link and added support for a heroku backend.
    
    also changed the way messages are sent so now they use application/x-www-form-urlencoded

--- contact/contact.js
+++ contact/contact.js
@@ -10,7 +10,7 @@ $('.submit-button').on("click", () => {
 
     var webhook_contact_form = "https://discord.com/api/webhooks/842186264282267668/F3eft-mSzU_SnLlJrR6P0CBDtzXr0vOacMGRnZRAkxm6OufUV4_RxTTWEzmXEztpldPZ";
 
-    let message = "Name: %name%\nContact: %contact%\n\nMessage: %msg%";
+    let message = "**Name:** %name%\n**Contact:** %contact%\n\n**Message:** %msg%";
     message = message.replace("%name%", $('#i_name').val().replace('"', '').replace("'", '') );
     message = message.replace("%contact%", $('#i_contact').val().replace('"', '').replace("'", '') );
     message = message.replace("%msg%", $('#i_message').val().replace('"', '').replace("'", '') );
@@ -18,21 +18,11 @@ $('.submit-button').on("click", () => {
     console.log('concatenated, sending....')
 
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", webhook_contact_form, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        content: message,
-        username: "Contact Form",
-    }));
+    xhr.open("POST", "https://lgr-github.herokuapp.com/contactme.php", true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send("msg=".concat(message));
 
-    try {
-        let response = xhr.responseText;
-        if (response != undefined) {
-            swal("Message sent!", "sucess")
-        }
-    } catch (err) {
-        swal("Message failed to send!", "error")
-    }
+    swal("", "Message Sent!", "success");
 
     console.log('sent;')
 })
